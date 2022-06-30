93234ed2e324e932b2de7312faa611e4ff7faceb
Andrey <93935023+desertptu4ka@users.noreply.github.com>
2022-07-01 01:05:07 +0300

    add basis scss, axios and lodash module.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+import * as axios from 'axios';
+import throttle from 'lodash/throttle';
+import debounce from 'lodash/throttle';
+
+console.log(axios);
+console.log(throttle);
+console.log(debounce);
