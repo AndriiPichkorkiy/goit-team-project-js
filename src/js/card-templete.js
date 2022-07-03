5b5dce3bccf0fd501a4518f99c5aac68ba18d0ea
orch20 <94862153+orch20@users.noreply.github.com>
2022-07-03 21:32:05 +0300

    added render card function

--- src/js/card-templete.js
+++ src/js/card-templete.js
@@ -2,38 +2,41 @@ import MovieService from '../js/movie-service';
 const movieService = new MovieService();
 const list = document.querySelector('.movies-card');
 
-// let year;
-function renderCardTemplate(data) {
-  console.log(data);
-  const { title, poster_path, release_date } = data;
+export default function renderCardTemplate(data) {
+  const { id, title, poster_path, release_date, vote_average, genre_ids } =
+    data;
 
-  card = `<li class="movies-card__item">
+  return `<li class="movies-card__item">
           <a
             href="#"
             class="movies-card__link"
             data-modal-open
-            data-card-id="тут id фільму"
+            data-card-id="${id}"
           >
             <div class="movies-card__thumb">
               <img
                 src="https://image.tmdb.org/t/p/w500/${poster_path}"
-                alt=""
+                alt="${title}"
                 loading="lazy"
               />
             </div>
             <div class="movies-card__content">
               <h2 class="movies-card__heading">${title}</h2>
               <p class="movies-card__text">
-                Drama, Action | ${release_date}
-                <span class="movies-card__rating">9.6</span>
+                ${genre_ids} | ${release_date}
+                <span class="movies-card__rating">${vote_average}</span>
               </p>
             </div>
           </a>
         </li>`;
-  console.log(list);
-  list.insertAdjacentHTML('beforeend', card);
 }
 
 movieService
   .getPopularMovies()
-  .then(data => data.results.map(data => renderCardTemplate(data)));
+  .then(data => data.results.map(data => renderCollection(data)));
+
+function renderCollection(data) {
+  const card = renderCardTemplate(data);
+
+  list.insertAdjacentHTML('beforeend', card);
+}
