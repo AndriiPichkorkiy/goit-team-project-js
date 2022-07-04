c67c310c45a239c66bb073406df047110757af1a
Nikita P <94718260+Akvigi@users.noreply.github.com>
2022-07-04 14:44:50 +0300

    1

---
+++ src/js/loading.js
@@ -0,0 +1,30 @@
+const iconList = document.querySelector('.icon-list');
+const form = document.querySelector('.search-form');
+const array = [...iconList.children];
+function search(e) {
+  e.preventDefault();
+  array.forEach(icon => {
+    icon.classList.remove(`is-hidden`);
+    icon.classList.add(`animate__zoomIn`);
+    let svgAr = icon.children;
+    let svg = svgAr[0];
+    console.log(svg);
+    // svg.classList.add(`animate__flash`);
+    // svg.classList.add(`animate__delay-5s`);
+    setTimeout(() => {
+      console.log('sdffdfg');
+      icon.classList.remove(`animate__zoomIn`);
+      for (let i = 0; i < array.length; i++) {
+        array[i].classList.remove(`animate__delay-${i}s`);
+      }
+      icon.classList.add(`animate__zoomIn`);
+      for (let i = 0; i < array.length; i++) {
+        array[i].classList.add(`animate__delay-${i}s`);
+      }
+    }, 5000);
+  });
+  for (let i = 0; i < array.length; i++) {
+    array[i].classList.add(`animate__delay-${i}s`);
+  }
+}
+form.addEventListener('submit', search);
