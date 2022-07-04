8e537f0b6b5d2b1b978fe5f87bc483035618e801
Anastasiia Lytviak <lav21051991@gmail.com>
2022-07-05 01:33:54 +0300

    fix foo fetchData

--- src/js/search-form.js
+++ src/js/search-form.js
@@ -2,56 +2,58 @@ import throttle from 'lodash/throttle';
 import debounce from 'lodash/throttle';
 import refs from "./refs";
 import MovieService from './movie-service';
+import renderCardTemplate from './card-templete';
+import './header'
 
-const newsApiService = new MovieService();
-console.log(newsApiService);
-const DEBOUNCE_DELAY = 300;
+refs.searchForm = document.querySelector(['.search-form']);
+
+const movieService = new MovieService();
+// const DEBOUNCE_DELAY = 300;
 
 // refs.searchForm.addEventListener("input", debounce(searchMovies, DEBOUNCE_DELAY));
 refs.searchForm.addEventListener("submit",searchMovies);
-// refs.searchBtn.addEventListener('click', searchMovies);
 
 function searchMovies(event) {
     event.preventDefault();
 
-    newsApiService.query = event.currentTarget.elements.query.value.trim();
+   const value = event.currentTarget.elements.query.value.trim();
     
-    console.log(newsApiService.query);
-    // if (newsApiService.query.length <= 2 || newsApiService.query.length === 0) {
-    //     moreTwoCharacters();
-    // }
-    // if (!newsApiService.query) {
-    //     correctionRequest();
-    // }
-    
-    fetchData();
-    // clearMarkup();
+    if (value.length <= 2 || value.length === 0) {
+        moreTwoCharacters();
+        return;
+    }
+
+    movieService.resetPage();
+
+    fetchData(value);
+
+    clearMarkup();
 }
-async function fetchData() {
-    try {
-        const data = await newsApiService.fetchMovies({})
-    console.log(data);
-    } catch (error) {
-    console.log(error);
-  }
-    
+
+async function fetchData(value) {
+    const data = await movieService.getMoviesByTitle(value)
+        .then(({ results }) => {
+            return results.map(result => renderCardTemplate(result))
+        })
+    refs.moviesCard.innerHTML = data;
 }
-function fetchError() {
-    alert('Oops!!!');
-}
+
+
 function moreTwoCharacters(){
     alert('Please enter more than 2 characters');
 }
-function correctionRequest(){
-    alert('Please enter a correction request');
-}
-function nothingRequest(){
-    alert('Sorry, there is nothing for your request');
-}
-function selectionMovies(){
-    alert('We have selected ${} movies for you. Enjoy yourself');
-}
-// function clearMarkup() {
-//   refs..innerHTML = '';
-//   refs..innerHTML = '';
+// function correctionRequest(){
+//     alert('Please enter a correction request');
 // }
+// function nothingRequest(){
+//     alert('Sorry, there is nothing for your request');
+// }
+// function selectionMovies(){
+//     alert('We have selected ${} movies for you. Enjoy yourself');
+// }
+// function fetchError() {
+//     alert('Oops!!!');
+// }
+function clearMarkup() {
+  refs.moviesCard.innerHTML = '';
+}
