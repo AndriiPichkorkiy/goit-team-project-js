2b0ef5d1fb12899c54ec4738800e72aaa8cf259c
olypchuk <eragmont@gmail.com>
2022-07-06 02:03:30 +0300

    create arrays of obj in LS

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,23 +1,42 @@
-import movieService from './movie-service'
+import MovieService from './movie-service'
+const movieService = new MovieService;
+// import onClickFilm from './film-modal'
+let idRandomNumber = Math.ceil(Math.random() * 30)
 
-import onClickFilm from './film-modal'
-console.log(onClickFilm);
+onClickFilm(idRandomNumber);
+async function onClickFilm(id) {
+    const answer = await movieService.getOneMovie(id).then(movie => {
+        // console.log(movie);
+        const markup = `<div class="film-card">
+        <div class="film-poster">
+       <a href="${movie.poster_path}">
+<img class="film-poster-img" src="${movie.poster_path}" alt="${movie.tags}" loading="lazy" width=375/>
+</div>
 
+<h2 class="name-film">${movie.original_title}</h2>
+<p class="info-film">Vote / Votes<span class="info-value">${movie.vote_average}</span></p>
+<p class="info-film">Popularity<span class="info-value">${movie.vote_count}</span></p>
+<p class="info-film">Popularity<span class="info-value">${movie.popularity}</span></p>
+<p class="info-film">Original Title<span class="info-value">${movie.original_title}</span></p>
+<p class="info-film">ABOUT<span class="info-value">${movie.overview}</span></p>
+
+<button type="button" class="add-to-watched">ADD TO WATCHED</button>
+<button type="button" class="add-to-queue">ADD TO QUEUE</button>
+</div>`
+      document.querySelector('.modal-film').insertAdjacentHTML('beforeend', markup);
+      return movie
+    }).then(data=>objInStorage(data))
+}
 //  <button type="button" class="add-to-watched">ADD TO WATCHED</button>
 // <button type="button" class="add-to-queue">ADD TO QUEUE</button> 
-// console.log(filmModal);
-// const movi = new MovieService
+//  <div class="wrapper-button">
+//       <button id="watched" class="watched-btn add-to-watched">add to watched</button>
+//   <button id="queue" class=" watched-btn queue-btn add-to-queue">add to queue</button></div>
+
 // // ..........мінімальна розмітка потрібна щоб працювало........
-//   ........................................................
-// .............................................................
-// <div class="wrapper-button">
-//         <button id ="watched" class="watched-btn">add to watched</button>
-//         <button id="queue" class=" watched-btn queue-btn">add to queue</button></div >
-// ................................................................................................................................................          
+     
 
 
-/////імітую айді різних карток
-let idRandomNumber = Math.ceil(Math.random() * 30)
 
 
 const watchedBtn = document.querySelector(".add-to-watched");
@@ -26,7 +45,8 @@ if (watchedBtn) {
   watchedBtn.addEventListener('click', addToWatched)
   queueBtn.addEventListener('click',addedToQueue)
 }
-
+let idInStorage;
+let objectInStorage;
 
 
 const localStorageKeys = {
@@ -34,6 +54,23 @@ const localStorageKeys = {
   filmInQueue:'filmInQueue'
 }
 
+function objInStorage(data) {
+  let obj;
+  const { id, title, poster_path, release_date, vote_average, genre_ids } =
+    data;
+
+  obj = {
+    id,
+    title,
+    poster_path,
+    release_date,
+    vote_average,
+    genre_ids
+  }
+ idInStorage = id
+  objectInStorage=obj
+  return obj
+}
 
 ////дістати значення з локалсторедж JSON.parse(localStorage.getItem(localStorageKeys.filmInQueue або localStorageKeys.watchedFilm
 
@@ -45,6 +82,7 @@ function addToWatched() {
     queueBtn.style.opacity = 0.25;
   
     watchedBtn.addEventListener('click', removeFromWatched)
+    watchedBtn.removeEventListener('click', addToWatched)
 }
 function removeFromWatched() {
   if (watchedBtn.textContent === 'remove from watched') {
@@ -55,7 +93,8 @@ function removeFromWatched() {
       queueBtn.style.opacity = 1;
 
     }
-     watchedBtn.removeEventListener('click', removeFromWatched)
+  watchedBtn.removeEventListener('click', removeFromWatched)
+  watchedBtn.addEventListener('click', addToWatched)
 }
 
 function addedToQueue() {
@@ -84,37 +123,22 @@ function removeFromQueue() {
 
 function jsonLocalStorage(value) {
   const oldItems = JSON.parse(localStorage.getItem(value)) || [];
-  if (!oldItems.includes(idRandomNumber)) {
-      oldItems.push(idRandomNumber);
-  }
-  localStorage.setItem(value, JSON.stringify(filterById(oldItems))); 
- 
+  oldItems.push(objectInStorage)
+  localStorage.setItem(value, JSON.stringify(oldItems));
 }
+   
+  
+
 function removeFromStorage(value) {
   const arr = JSON.parse(localStorage.getItem(value))
-  if (arr.includes(idRandomNumber)){
-    removeItemAll(arr,idRandomNumber)
-    localStorage.setItem(value, JSON.stringify(filterById(arr)))
-    console.log(filterById(arr));
-   
-  }
+  const newArray=filterArrObj(arr)
+  localStorage.setItem(value, JSON.stringify(newArray))
 
 
 }
 
+function filterArrObj(arr) {
 
-function filterById(arr) {
- return arr.filter(
-    (film, index, array) => array.indexOf(film) === index
-  );
-
-}
-
-function removeItemAll(arr, value) {
-  for (let i = 0; i < arr.length; i+=1) {
-    if (arr[i] === value) {
-      arr.splice(i, 1);
-    } 
-  }
-  return arr
+  const newArr = arr.filter(film => film.id !== idInStorage)
+  return newArr 
 }
