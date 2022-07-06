8f0536b31f95f7c2b1baa06a1843dd1f8c863cf9
Andrey <93935023+desertptu4ka@users.noreply.github.com>
2022-07-06 14:32:58 +0300

    fix import movieService beacouse of change logic export this module

--- src/js/film-modal.js
+++ src/js/film-modal.js
@@ -1,36 +1,31 @@
-import { result } from "lodash";
-import MovieService from "./movie-service";
+import { result } from 'lodash';
+import { movieService } from './movie-service';
 
-const movieService = new MovieService;
-
+// const movieService = new MovieService;
 
 const refs = {
-    filmModal: document.querySelector('.modal-film'),
-    closeModalFilm: document.querySelector('.modal-film-close__btn'),
-    backdropFilmModal: document.querySelector('.js-backdrop-film-modal'),
+  filmModal: document.querySelector('.modal-film'),
+  closeModalFilm: document.querySelector('.modal-film-close__btn'),
+  backdropFilmModal: document.querySelector('.js-backdrop-film-modal'),
 };
 
-
-refs.closeModalFilm.addEventListener('click', onCloseModal)
+refs.closeModalFilm.addEventListener('click', onCloseModal);
 refs.backdropFilmModal.addEventListener('click', onBackDropClick);
 
-
-
 function onCloseModal() {
-    document.body.classList.remove('show-modal');
+  document.body.classList.remove('show-modal');
 }
 
 function onBackDropClick(event) {
-    if (event.currentTarget === event.target) {
-     onCloseModal()
-    }
+  if (event.currentTarget === event.target) {
+    onCloseModal();
+  }
 }
 
-
 export default async function onClickFilm(id) {
-    const answer = await movieService.getOneMovie(id).then(movie => {
-        console.log(movie);
-        const markup = `<div class="film-card">
+  const answer = await movieService.getOneMovie(id).then(movie => {
+    console.log(movie);
+    const markup = `<div class="film-card">
         <div class="film-poster">
        <a href="${movie.poster_path}">
 <img class="film-poster-img" src="${movie.poster_path}" alt="${movie.tags}" loading="lazy" width=375/>
@@ -45,8 +40,8 @@ export default async function onClickFilm(id) {
 
 <button type="button" class="add-to-watched">ADD TO WATCHED</button>
 <button type="button" class="add-to-queue">ADD TO QUEUE</button>
-</div>`
-        refs.filmModal.insertAdjacentHTML('beforeend', markup);
-    })
+</div>`;
+    refs.filmModal.insertAdjacentHTML('beforeend', markup);
+  });
 }
 // onClickFilm(453395);
