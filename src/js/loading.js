3d1be7ccd0e0a120d56faebb146f2fc9981233b3
Nikita P <94718260+Akvigi@users.noreply.github.com>
2022-07-06 23:10:44 +0300

    changed 2 func to 1 obj with 2 methods

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,37 +1,40 @@
 const iconList = document.querySelector('.icon-list');
 const form = document.querySelector('.search-form');
 const array = [...iconList.children];
-export function animationWhenSearch(e) {
-  e.preventDefault();
-  array.forEach(icon => {
-    icon.classList.remove(`is-hidden`);
-    icon.classList.add(`animate__zoomIn`);
-    let svgAr = icon.children;
-    let svg = svgAr[0];
-    svg.classList.add(`animate__flash`);
-    svg.classList.add(`animate__delay-5s`);
-  });
-  for (let i = 0; i < array.length; i++) {
-    array[i].classList.add(`animate__delay-${i}s`);
-  }
-}
+
+export const loading = {
+  on: function (e) {
+    e.preventDefault();
+    array.forEach(icon => {
+      icon.classList.remove(`is-hidden`);
+      icon.classList.add(`animate__zoomIn`);
+      let svgAr = icon.children;
+      let svg = svgAr[0];
+      svg.classList.add(`animate__flash`);
+      svg.classList.add(`animate__delay-5s`);
+    });
+    for (let i = 0; i < array.length; i++) {
+      array[i].classList.add(`animate__delay-${i}s`);
+    }
+  },
+  off: function () {
+    if (iconList.classList.contains(`blockSreen`)) {
+      iconList.classList.remove(`blockSreen`);
+    }
+    array.forEach(icon => {
+      icon.classList.add(`is-hidden`);
+      icon.classList.remove(`animate__zoomIn`);
+      let svgAr = icon.children;
+      let svg = svgAr[0];
+      svg.classList.remove(`animate__flash`);
+      svg.classList.remove(`animate__delay-5s`);
+    });
+    for (let i = 0; i < array.length; i++) {
+      array[i].classList.remove(`animate__delay-${i}s`);
+    }
+  },
+};
+
 export function blockSreen() {
   iconList.classList.add(`blockSreen`);
-}
-
-export function removeAnimation() {
-  if (iconList.classList.contains(`blockSreen`)) {
-    iconList.classList.remove(`blockSreen`);
-  }
-  array.forEach(icon => {
-    icon.classList.add(`is-hidden`);
-    icon.classList.remove(`animate__zoomIn`);
-    let svgAr = icon.children;
-    let svg = svgAr[0];
-    svg.classList.remove(`animate__flash`);
-    svg.classList.remove(`animate__delay-5s`);
-  });
-  for (let i = 0; i < array.length; i++) {
-    array[i].classList.remove(`animate__delay-${i}s`);
-  }
 }
