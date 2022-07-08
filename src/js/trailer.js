d1c56e44c4020354fc9e089168fb6156f3dfed04
AnnaNesterenko <nura_arh@ukr.net>
2022-07-08 19:44:01 +0300

    trailer/js/created

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -1,17 +1,48 @@
-export default function wоchMovie() {
-  const videos = v.results;
-  console.log('-------функція wochMovie--------');
-  const btn = document.querySelector('.add-to-watched');
-  console.log('Arr: ', videos);
-  console.log('1');
-  console.log(btn);
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
 
-  btn.addEventListener('click', () => {
-    woch(videos);
+let buttonTrailer;
+
+export default function addEventsOnModalBtnTrailer(el) {
+  const videos = el.results;
+  console.log('-------функція watchMovie--------');
+  buttonTrailer = document.querySelector('.btn-trailer');
+
+  buttonTrailer.addEventListener('click', () => {
+    watchToTrailer(videos);
   });
 }
 
-function woch(videos) {
-  console.log("You click to button 'Woch movie'!");
-  console.log('Arr: ', videos);
+function watchToTrailer(videos) {
+  console.log("You click to button 'Watch movie'!");
+  console.log('videos: ', videos);
+  const video = videos.find(el => {
+    return el.site === 'YouTube';
+  });
+
+  const key = video.key;
+  console.log(key);
+
+  const instance = basicLightbox.create(`
+    <iframe src="https://www.youtube.com/embed/${key}" width="560" height="315" frameborder="0"></iframe>
+`);
+  instance.show();
+  modalClBtTrailer();
+
+  if (!video) {
+    return alert('not video');
+  }
+}
+
+function modalClBtTrailer(instance) {
+  const modalBox = document.querySelector('.basicLightbox--iframe');
+  modalBox.insertAdjacentHTML(
+    'afterbegin',
+    `<button
+        type="button"
+        class="lightbox__button"
+        data-action="close-lightbox"
+        ></button>
+    `
+  );
 }
