6ad0bfa60bde859c4063da0758651e8c6c30fc78
AnnaNesterenko <nura_arh@ukr.net>
2022-07-09 14:08:40 +0300

    bagFix/header&trailer

--- src/js/header.js
+++ src/js/header.js
@@ -1,100 +1,9 @@
-// import refs from '/src/js/refs';
-// import * as iconSearch from '/src/images/icons.svg';
-// import {activateHeadersBtn} from './myLibraryBtns'
-// const activeHeader = document.querySelector('.js-active-header');
-// const logoPageHome = document.querySelector('.logo__name');
-// let mainPage = 'homePage';
-
-// markupForm();
-
-// refs.home.classList.add('side-nav__link--current');
-// refs.logoLink.setAttribute('style', 'cursor:default');
-// logoPageHome.classList.add('reveal-text');
-
-// refs.home.addEventListener('click', e => {
-//   e.preventDefault();
-//   if (mainPage === 'homePage') return;
-
-//   markupPageHome();
-//   markupForm();
-// });
-
-// refs.library.addEventListener('click', e => {
-//   e.preventDefault();
-//   if (mainPage === 'libraryPage') return;
-//   markupPageLibrary();
-//   markupButtons();
-// });
-
-// refs.logoLink.addEventListener('click', e => {
-//   e.preventDefault();
-//   if (mainPage !== 'libraryPage') return;
-//   markupPageHome();
-//   markupForm();
-// });
-
-// function markupPageHome() {
-//   mainPage = 'homePage';
-//   refs.home.classList.add('side-nav__link--current');
-//   refs.library.classList.remove('side-nav__link--current');
-//   refs.header.classList.remove('header-bg-lib');
-//   refs.logoLink.setAttribute('style', 'cursor:default');
-//   logoPageHome.classList.add('reveal-text');
-//   logoPageHome.classList.remove('rise-text');
-// }
-
-// function markupPageLibrary() {
-//   mainPage = 'libraryPage';
-//   refs.library.classList.add('side-nav__link--current');
-//   refs.home.classList.remove('side-nav__link--current');
-//   refs.header.classList.add('header-bg-lib');
-//   refs.logoLink.setAttribute('style', 'cursor:pointer;');
-//   logoPageHome.classList.remove('reveal-text');
-//   logoPageHome.classList.add('rise-text');
-// }
-
-// function markupForm() {
-//   activeHeader.innerHTML = ` <form class="search-form" data-id="searchForm">
-//           <input
-//             type="text"
-//             name="query"
-//             autocomplete="off"
-//             data-id="searchQuery"
-//             placeholder="Movie search"
-//             class="search-field"
-//           />
-//           <button type="submit" class="search-btn" data-id="searchBtn">
-//             <svg class="search-icon" width="12" height="12">
-//               <use href="${iconSearch}#icon-search"></use>
-//             </svg>
-//           </button>
-//         </form>`;
-// }
-// export function markupButtons() {
-//   activeHeader.innerHTML = `<div class="buttons-wrapper"><button
-//             type="button"
-//             class="library__buttons library__button--active library__button--trailer"
-//             data-id="watchedBtn"
-//           >
-//             Watched
-//           </button>
-//           <button type="button" class="library__buttons" data-id="queueBtn">
-//             Queue
-//           </button></div>`;activateHeadersBtn()
-// }
 import refs from '/src/js/refs';
 import * as iconSearch from '/src/images/icons.svg';
-import {
-  doChangeThemeBtnMarkup,
-  addEvLisToThemeBtn,
-  changeTo,
-  currentTheme,
-} from './dark-theme.js';
-
+import { activateHeadersBtn } from './myLibraryBtns';
 const activeHeader = document.querySelector('.js-active-header');
 const logoPageHome = document.querySelector('.logo__name');
 let mainPage = 'homePage';
-const changeBtnMarkup = doChangeThemeBtnMarkup();
 
 markupForm();
 
@@ -145,36 +54,27 @@ function markupPageLibrary() {
 }
 
 function markupForm() {
-  activeHeader.innerHTML = ` 
-        <div class="form-and-changebtn">
-		   ${changeBtnMarkup}
-          <form class="search-form" data-id="searchForm">
-            <input
-              type="text"
-              name="query"
-              autocomplete="off"
-              data-id="searchQuery"
-              placeholder="Movie search"
-              class="search-field"
-            />
-            <button type="submit" class="search-btn" data-id="searchBtn">
-              <svg class="search-icon" width="12" height="12">
-                <use href="${iconSearch}#icon-search"></use>
-              </svg>
-            </button>
-          </form>
-        `;
-  addEvLisToThemeBtn();
-  if (localStorage.getItem('THEME') === 'DARK') {
-    changeTo.dark();
-    currentTheme.changeToDark();
-  }
+  activeHeader.innerHTML = ` <form class="search-form" data-id="searchForm">
+          <input
+            type="text"
+            name="query"
+            autocomplete="off"
+            data-id="searchQuery"
+            placeholder="Movie search"
+            class="search-field"
+          />
+          <button type="submit" class="search-btn" data-id="searchBtn">
+            <svg class="search-icon" width="12" height="12">
+              <use href="${iconSearch}#icon-search"></use>
+            </svg>
+          </button>
+        </form>`;
 }
 
-function markupButtons() {
+export function markupButtons() {
   activeHeader.innerHTML = `<div class="buttons-wrapper"><button
             type="button"
-            class="library__buttons library__button--active"
+            class="library__buttons library__button--active library__button--trailer"
             data-id="watchedBtn"
           >
             Watched
@@ -182,4 +82,6 @@ function markupButtons() {
           <button type="button" class="library__buttons" data-id="queueBtn">
             Queue
           </button></div>`;
+  activateHeadersBtn();
 }
+
